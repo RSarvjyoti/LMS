fc0e34b5a5089dadbc7062642d2a854582df3121
RSarvjyoti <sarvjyoti64@gmail.com>
2025-03-22 18:57:52 +0530

    added email validation from backend

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -6,12 +6,17 @@ require("dotenv").config();
 const signup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
-    let photoUrl = "";
 
+    let photoUrl = "";
     if (req.file) {
       photoUrl = req.file.path;
     }
@@ -25,13 +30,15 @@ const signup = async (req, res) => {
       password: hashedPassword,
       photo: photoUrl,
     });
-    await newUser.save();
 
+    await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
+
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
   }
 };
+
 
 const login = async (req, res) => {
   try {
